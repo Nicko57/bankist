ba3d4c8f07f7034cd45cce29a382be9508663ee1
Nickolas <work35955@gmail.com>
2021-12-01 13:30:35 -0800

    Project Setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./index.css";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h2 className="text-red-600 text-3xl">Portfolio project...</h2>
+    </div>
+  );
+}
+
+export default App;
